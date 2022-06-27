34aefddac4d4a9cfbe7d17a93ebecac49043fac4
Fabricio Castro <baptistafabricio87@gmail.com>
2022-06-27 10:46:13 -0300

    Adicionando novas views

--- receitas/views.py
+++ receitas/views.py
@@ -2,5 +2,19 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    receitas = {
+        1:'Lasanha',
+        2:'Sorvete',
+        3:'Sopa de Legumes',
+        4:'Caldo Verde',
+        5:'Bolo de Chocolate'
+    }
+
+    dados = {
+        'nomes_receitas' : receitas
+    }
+    return render(request, 'index.html', dados)
+
+def receita(request):
+    return render(request, 'receita.html')
 
