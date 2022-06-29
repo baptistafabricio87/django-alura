affb5ca2a92548125c500c9044bd544850b83fd6
Fabricio Castro <baptistafabricio87@gmail.com>
2022-06-29 15:20:26 -0300

    Adicionado função para uploade de foto da receita

--- receitas/admin.py
+++ receitas/admin.py
@@ -4,11 +4,12 @@ from .models import Receita
 
 # Register your class filter
 class ListReceitas(admin.ModelAdmin):
-    list_display = ('id', 'nome_receita', 'categoria', 'pessoa')
+    list_display = ('id', 'nome_receita', 'categoria', 'pessoa', 'publicar')
     list_display_links = ('id', 'nome_receita')
     search_fields = ('nome_receita',)
     list_filter = ('categoria',)
-    list_per_page: 2
-
+    list_editable = ('publicar',)
+    list_per_page = 2
+    
 # Register your models here.
 admin.site.register(Receita, ListReceitas)
--- receitas/models.py
+++ receitas/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from distutils.command.upload import upload
 
 from django.db import models
 from pessoas.models import Pessoa
@@ -13,3 +14,5 @@ class Receita(models.Model):
     rendimento = models.CharField(max_length=100)
     categoria = models.CharField(max_length=100)
     data_receita = models.DateTimeField(default=datetime.now, blank=True)
+    foto_receita = models.ImageField(upload_to='fotos/%d/%m/%Y', blank=True)
+    publicar = models.BooleanField(default=False)
