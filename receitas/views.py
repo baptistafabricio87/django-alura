57e3dbbd4d2f15ae097f5db52f38ad304c5b1279
Fabricio Castro <baptistafabricio87@gmail.com>
2022-06-29 18:07:50 -0300

    Cofiguracao do 'static root' e 'settings'

--- receitas/views.py
+++ receitas/views.py
@@ -18,3 +18,15 @@ def receita(request, receita_id):
     receita_exibir = {"receita": receita}
 
     return render(request, "receita.html", receita_exibir)
+
+def buscar(request):
+    lista_receitas = Receita.objects.order_by('-data_receita').filter(publicar=True)
+    
+    if 'buscar' in request.GET:
+        nome_pesquisa = request.GET['buscar']
+        if buscar:
+            lista_receitas = lista_receitas.filter(nome_receita__icontains=nome_pesquisa)
+            
+    dados = {'receitas' : lista_receitas}
+        
+    return render(request, 'index.html', dados)
