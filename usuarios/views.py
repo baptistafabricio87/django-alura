53881a126e85e33cd37e22a1f755b899855a4228
Fabricio Castro <baptistafabricio87@gmail.com>
2022-06-30 15:01:35 -0300

    Novo App de usuarios, form de cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,14 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 def cadastro(request):
-    pass
+    if request.method == 'POST':
+        nome = request.POST['nome']
+        email = request.POST['email']
+        senha = request.POST['password']
+        senha2 = request.POST['password2']
+
+        if not nome.strip():
+            print('Nome não pode ficar em branco')
+            return redirect('cadastro')
+        if not email.strip():
+            print('E-mail não pode ficar em branco')
+            return redirect('cadastro')
+        if not senha.strip():
+            print('Senha não pode ficar em branco')
+            return redirect('cadastro')
+        if not senha2.strip():
+            print('Senha não pode ficar em branco')
+            return redirect('cadastro')
+        if not senha == senha2:
+            print('As senhas não são iguais')
+            return redirect('cadastro')
+        if User.objects.filter(email=email).exists():
+            print('Usuario já cadastrado!')
+            return redirect('cadastro')
+        else:
+            user = User.objects.create_user(
+                username=nome, email=email, password=senha)
+            user.save()
+            print(f'Usuario {nome} cadastrado com sucesso')
+            return redirect('login')
+    else:
+        return render(request, 'usuarios/cadastro.html')
+
 
 def login(request):
-    pass
+    return render(request, 'usuarios/login.html')
+
 
 def dashboard(request):
     pass
+
 
 def logout(request):
     pass
